d417732094f9672059f6db34a47bb48b59130ee1
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2026-04-07 21:53:48 -0500

    feat: Bug en color de fondo para la página de Planes.

--- src/features/plans/index.tsx
+++ src/features/plans/index.tsx
@@ -2,7 +2,7 @@ import PlansComponent from "./components/Plans"
 
 export default function Plans() {
   return (
-    <main className="pt-30 md:pt-40">
+    <main className="pt-30 md:pt-40 bg-[#1d232a]">
       <PlansComponent />
     </main>
   )
